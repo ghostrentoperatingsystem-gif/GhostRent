0b4883e2ab1457c22670cc493b1a185e56f5d49c
ghostrentoperatingsystem-gif <ghostrentoperatingsystem@gmail.com>
2026-07-29 15:57:48 +0200

    Update page.js

--- app/profile/page.js
+++ app/profile/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { createClient } from "../../lib/supabase";
+import { createClient } from "../../lib/supabase-browser";
 import { LogOut } from "lucide-react";
 
 export default function ProfilePage() {
